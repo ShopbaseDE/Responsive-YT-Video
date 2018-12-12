361b4ca1cc352acbf91cf3f95a62cdbea836bff9
Hendrik Legge <hendrik.legge@themepoint.de>
2018-12-12 09:12:50 +0100

    Update jquery.rytv.js

--- jquery.rytv.js
+++ jquery.rytv.js
@@ -1,14 +1,16 @@
 $.fn.rytv = function(options) {
-    var opts = $.extend( {}, $.fn.rytv.defaults, options );
- 	var size = (opts.size.split(':')[1] / opts.size.split(':')[0]) * 100;
- 	$(this).wrap('<div style="position: relative;padding-bottom: ' + size + '%;padding-top: 30px;height: 0;overflow: hidden;"></div>');
-	$(this).css({
+	var opts = $.extend( {}, $.fn.rytv.defaults, options );
+	var size = (opts.size.split(':')[1] / opts.size.split(':')[0]) * 100;
+	var $this = this;
+	$this.wrap('<div style="position: relative;padding-bottom: ' + size + '%;padding-top: 30px;height: 0;overflow: hidden;"></div>');
+	$this.css({
 		'position': 'absolute',
 		'top': 0,
 		'left': 0,
 		'width': '100%',
 		'height': 100%,
 	});
+	return this;
 };
 
 $.fn.rytv.defaults = {
